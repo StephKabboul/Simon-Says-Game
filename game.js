bd0b7e8d50138d48a3622d492327a76551f8c798
@StephKabboul <stephkabboul@gmail.com>
2024-06-29 17:18:06 +0300

    Created playSound function

--- game.js
+++ game.js
@@ -1,28 +1,36 @@
-// const buttonColors = ["red", "blue", "green", "yellow"]
-// let gamePattern = [];
-// let userClickedPattern = []
-// let started = false
-// let level = 0
+const buttonColors = ["red", "blue", "green", "yellow"]
+let gamePattern = [];
+let userClickedPattern = []
+let started = false
+let level = 0
 
-// document.addEventListener('keypress', () => {
+document.addEventListener('keypress', () => {
 
-//     if (!started) {
-//         document.querySelector("#level-title").textContent = `Level ${level}`
-//         nextSequence();
-//         started = true
-//     }
-// })
+    if (!started) {
+        document.querySelector("#level-title").textContent = `Level ${level}`
+        nextSequence();
+        started = true
+    }
+})
 
-// game.js
+function nextSequence() {
+    userClickedPattern = [];
+    level++
+    document.querySelector("#level-title").textContent = `Level ${level}`
+
+    const RandomButtonIndex = Math.floor(Math.random()*4);
+    const RandomChosenColor = buttonColors[RandomButtonIndex]
+
+    gamePattern.push(RandomChosenColor);
+    document.getElementById(RandomChosenColor).classList.add("pressed")
+    setTimeout(() => {
+        document.getElementById(randomChosenColor).classList.remove("pressed");
+    }, 100);
+    playSound(randomChosenColor);
+
+}
 
 
-        } else {
-            const userChosenColor = event.target.id;
-            userClickedPattern.push(userChosenColor);
-            playSound(userChosenColor);
-            animatePress(userChosenColor);
-            checkAnswer(userClickedPattern.length - 1);
-        }
-    });
-});
+
+
 
